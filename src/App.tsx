0f7488181486ef43e36ed619a315b8dc883c873a
yemarn <yemarn.510@gmail.com>
2026-07-13 13:19:17 +0700

    FE: added shadcn components, button, table, select and dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Nav from "./components/Navbar"
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <main>
       <Nav/>
+
+      <Button> testing </Button>
     </main>
   )
 }
